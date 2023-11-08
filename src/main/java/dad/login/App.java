e7d178b8e6315db047763845a5984b1d6e111f7f
David <david.diax66@gmail.com>
2023-11-08 12:41:06 +0000

    xdd

--- src/main/java/dad/login/App.java
+++ src/main/java/dad/login/App.java
@@ -1,7 +1,6 @@
 package dad.login;
 
-import dad.mvc.login.LoginController;
-import dad.mvc.login.LoginView;
+import dad.mvc.fxml.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,15 +8,16 @@ import javafx.stage.Stage;
 public class App extends Application {
 	
 	// controllers
-	private LoginController loginController = new LoginController();
+	private Controller controller = new Controller();
 
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 	
 		//ventana del login
+
 		
-		Scene scene = new Scene(loginController.getView(), 320, 200);
+		Scene scene = new Scene(controller.getView(), 320, 200);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Login");
 		primaryStage.show();
---
+++ src/main/java/dad/mvc/fxml/Controller.java
@@ -0,0 +1,123 @@
+package dad.mvc.fxml;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dad.login.auth.AuthService;
+import dad.login.auth.FileAuthService;
+import dad.login.auth.LdapAuthService;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Controller implements Initializable {
+
+	// model
+	private StringProperty usuarioValor = new SimpleStringProperty();
+	private StringProperty contrasenaValor = new SimpleStringProperty();
+	private BooleanProperty ldapValor = new SimpleBooleanProperty();
+	private AuthService servicioAutentificacion;
+
+	// view
+
+	@FXML
+	private Button accederButton;
+
+	@FXML
+	private HBox botonesHBox;
+
+	@FXML
+	private Button cancelarButton;
+
+	@FXML
+	private TextField contrasenaText;
+
+	@FXML
+	private CheckBox ldapCheckbox;
+
+	@FXML
+	private VBox view;
+
+	@FXML
+	private GridPane usuarioContrasenaPane;
+
+	@FXML
+	private TextField usuarioText;
+
+
+	public Controller() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginView.fxml"));
+			loader.setController(this);
+			loader.load();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		usuarioText.textProperty().bindBidirectional(usuarioValor);
+		contrasenaText.textProperty().bindBidirectional(contrasenaValor);
+		ldapCheckbox.selectedProperty().bindBidirectional(ldapValor);
+		
+
+	}
+	
+	public VBox getView() {
+		return view;
+	}
+	
+	public boolean autenticar() throws Exception {
+		if(ldapValor.get()) {
+			servicioAutentificacion = new LdapAuthService();
+		} else {
+			servicioAutentificacion = new FileAuthService();
+		}
+		
+		return servicioAutentificacion.login(null, null);
+	}
+	
+	@FXML
+	void onAccederAction(ActionEvent event) {
+		System.out.println(usuarioValor, contrasenaValor);
+		try {
+			if(autenticar()) {
+				Alert alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle("Iniciar sesión");
+				alert.setHeaderText("Acceso permitido");
+				alert.setContentText("Las credenciales de acceso son válidas");
+				alert.showAndWait();
+		
+			}
+			else {
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setTitle("Iniciar sesión");
+				alert.setHeaderText("Acceso denegado");
+				alert.setContentText("El usuario y/o la contraseña no son válidos.");
+				alert.showAndWait();
+		
+			}
+		} catch (Exception a) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Iniciar sesión");
+			alert.setHeaderText("Acceso denegado");
+			alert.setContentText("El usuario y/o la contraseña no son válidos.");
+			alert.showAndWait();
+		}
+	}
+}
